ff40e62dc8d6c141cbe4a0711fef28eee721df30
Admin <scadmin@clarington.net>
2021-06-14 16:30:38 -0400

    First commit

---
+++ src/webparts/claringtonStaffDirectory/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IClaringtonStaffDirectoryWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ClaringtonStaffDirectoryWebPartStrings' {
+  const strings: IClaringtonStaffDirectoryWebPartStrings;
+  export = strings;
+}
